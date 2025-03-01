356f4a3b582507a625c3a961249746d6a0afa296
embedded-kiddie <kings.unlikely@gmail.com>
2025-03-02 08:54:44 +0900

    Update comment.

--- gfx.hpp
+++ gfx.hpp
@@ -19,8 +19,8 @@
     #define LGFX_AUTODETECT
     #include <LovyanGFX.h>
     #else
-    // false: (micro-USB x 1 type, ILI9341)
-    // true : (micro-USB x 1 + USB-C x 1 type, ST7789)
+    // false: Panel driver: ILI9341 (micro-USB x 1 type)
+    // true : Panel driver: ST7789  (micro-USB x 1 + USB-C x 1 type)
     #define DISPLAY_CYD_2USB  true
     #include "./boards/LGFX_ESP32_2432S028R_CYD.hpp"
     #endif
